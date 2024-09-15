b1ed5ead49d682b6ea7ca384bffd3abf3edbfb37
Hiralal Singh <135902087+Root-acess@users.noreply.github.com>
2024-09-15 16:06:33 +0530

    Update PromptPage.tsx

--- src/Pages/PromptPage.tsx
+++ src/Pages/PromptPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import SlideBar from '../Component/Slidebar'; // Adjust the import path as needed
 
 const Aipage = () => {
